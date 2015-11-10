475263485fdf4e88777145269c898210ce050fad
Zack <thurston@cwolf.cs.sonoma.edu>
2015-11-10 11:54:39 -0800

    linked login, homepage, both profiles, and edit profile

---
+++ LoginRegister/app/src/main/java/com/example/student/loginregister/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.student.loginregister;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+@SuppressWarnings("deprecation")
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    Button bLogout;
+    EditText etName, etAge, etUsername;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etName = (EditText) findViewById(R.id.etName);
+        etAge = (EditText) findViewById(R.id.etAge);
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        bLogout = (Button) findViewById(R.id.bRegister);
+
+        bLogout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.bLogin:
+
+                startActivity(new Intent(this, Login.class));
+
+                break;
+        }
+    }
+}
