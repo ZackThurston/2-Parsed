64b9580a000b17231c0e1e892f717bea9d99dc7c
morenodr <morenodr@sonoma.edu>
2015-11-10 16:23:49 -0800

    Register works, but login display doesn't

---
+++ app/src/main/java/com/example/bxr/loginregister/Register.java
@@ -0,0 +1,61 @@
+package com.example.bxr.loginregister;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Register extends AppCompatActivity implements View.OnClickListener{
+
+    Button bRegister;
+    EditText etFirstName, etLastName, etEmail, etPassword;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        etFirstName = (EditText) findViewById(R.id.etFirstName);
+        etLastName = (EditText) findViewById(R.id.etLastName);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        bRegister = (Button) findViewById(R.id.bRegister);
+
+        bRegister.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.bRegister:
+
+                String first_name = etFirstName.getText().toString();
+                String last_name = etLastName.getText().toString();
+                String email = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
+
+                User user = new User(first_name, last_name, email, password);
+
+                registerUser(user);
+
+                break;
+        }
+    }
+
+    private void registerUser(User user){
+        ServerRequest serverRequest = new ServerRequest(this);
+        serverRequest.storeUserDataInBackground(user, new GetUserCallback() {
+            @Override
+            public void done(User returnedUser) {
+                startActivity(new Intent(Register.this, Login.class));
+            }
+        });
+    }
+}
